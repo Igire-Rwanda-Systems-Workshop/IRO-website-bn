35c2b29aff092cd2ba52f8e0dd9276c0986ee0b7
kezagiselle <kezagiselle776@gmail.com>
2024-10-15 09:44:56 +0200

    done my ProgramAPIs

--- contentManagementSystem/routes/programRoutes.js
+++ contentManagementSystem/routes/programRoutes.js
@@ -1,17 +1,9 @@
 import programController from "../controllers/ProgramController.js";
 import express from "express";
 const programRouter = express.Router();
-import { upload } from '../../middleware/multer.js';
+// import { upload } from '../../middleware/multer.js';
 
-programRouter.post(
-    '/upload',
-    upload.fields([
-      { name: 'backgroundImage1', maxCount: 1 },
-      { name: 'backgroundImage2', maxCount: 1 },
-      { name: 'backgroundImage3', maxCount: 1 }
-    ]),
-    programController.createProgram
-  );
+  programRouter.post('/create', programController.createProgram);
   programRouter.get('/getAll', programController.getAllPrograms);
   programRouter.get('/getById/:id', programController.getProgramById);
   programRouter.put('/update/:id', programController.updateProgram);
