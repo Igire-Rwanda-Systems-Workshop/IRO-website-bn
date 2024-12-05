b8871a682fb435360007c6bdfd735fc79f861358
Iracyadukunda Angelique <angeiracyadukunda@gmail.com>
2024-12-05 13:02:30 +0200

    swagger

--- email/sendmail.js
+++ email/sendmail.js
@@ -6,7 +6,7 @@ dotenv.config()
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, // true for port 465, false for other ports
+    secure: false, 
     auth: {
       user: `${process.env.USER_EMAIL}`,
       pass: `${process.env.APP_PASSWORD}`,
--- server.js
+++ server.js
@@ -4,12 +4,23 @@ import connection from "./db/connection.js"
 import router from "./routes/router.js"
 import swaggerUi from "swagger-ui-express";
 import swagger from "./docs/swagger.json" with { type: "json" };
+import cors from "cors"
 
 dotenv.config()
 
 const app = new express()
+
 app.use(express.json())
 app.use( router )
+// cors
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://iro-employee-bn.onrender.com/'], // Add your deployed URL
+    credentials: true, // Allow credentials like cookies
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    methods: ['GET', 'POST', 'PUT', 'UPDATE', 'DELETE'],
+};
+app.use( cors() )
+
 // Swagger Documentation
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger));
 
