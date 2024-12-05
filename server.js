641e1309edd8d9a9a959943ea8d38f12bbed4eb8
Iracyadukunda Angelique <angeiracyadukunda@gmail.com>
2024-12-05 12:00:20 +0200

    assert

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 import connection from "./db/connection.js"
 import router from "./routes/router.js"
 import swaggerUi from "swagger-ui-express";
-import swagger from "./docs/swagger.json" assert { type: "json" };
+// import swagger from "./docs/swagger.json" assert { type: "json" };
 
 dotenv.config()
 
@@ -11,7 +11,7 @@ const app = new express()
 app.use(express.json())
 app.use( router )
 // Swagger Documentation
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger));
 
 
 
